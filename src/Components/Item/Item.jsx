3cb5636e98419fc74387e74fe08690f1551860cf
Robinson Yevilao <robinsonyevilao@gmail.com>
2024-01-08 23:05:38 -0300

    cambios V3

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import { Button } from "../Button/Button"
 import styles from "./item.module.css"
 
-export const Item = ({ name, price, img }) => {
+export const Item = ({ id, name, price, img }) => {
     return (
         <div className={styles.item}>
             <img src={img} alt="producto" />
